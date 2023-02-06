feecbfc53bdc048749d15d1e38aa008833293acb
Kavume <e.gorbacheva404@gmail.com>
2023-02-06 13:20:33 +0100

    feat: add footer component, normalize styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React, {ReactNode} from "react";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Layout({children}:{children: ReactNode}) {
     return (
         <>
             <Navbar />
             <main>{children}</main>
+            <Footer />
         </>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from 'next/app'
 import Layout from "@/components/Layout/Layout";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@/pages/global_styles.scss';
 
 const queryClient = new QueryClient();
 
