7c2b5a184173e1f93b3b1856cced342924527b4f
Lana <tiushkievich00@mail.ru>
2023-02-15 11:53:08 +0100

    feat: add google button

--- src/components/AuthForm/SignUpForm/SignUpForm.tsx
+++ src/components/AuthForm/SignUpForm/SignUpForm.tsx
@@ -5,19 +5,24 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { validationUserSignUp } from "./schema";
 import Input from "@/components/common/Input";
-import Button from '@/components/common/Button';
-import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
-import { useAppDispatch, useAppSelector } from '@/store/store';
-import { main } from '@/store/selectors';
-import { setUserDisplayName, setUserEmail } from '@/slices/main';
-import ErrorLabel from '../../common/ErrorLabel/ErrorLabel';
+import Button from "@/components/common/Button";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "firebase/auth";
+import { useAppDispatch, useAppSelector } from "@/store/store";
+import { main } from "@/store/selectors";
+import { setUserDisplayName, setUserEmail } from "@/slices/main";
+import ErrorLabel from "../../common/ErrorLabel/ErrorLabel";
+import GoogleButton from "@/components/Header/GoogleButton";
 
 interface ISignUpForm {
-  toggleForm : () => void;
+  toggleForm: () => void;
   closeModal: () => void;
 }
 
-const SignUpForm = ({closeModal, toggleForm}: ISignUpForm) => {
+const SignUpForm = ({ closeModal, toggleForm }: ISignUpForm) => {
   const dispatch = useAppDispatch();
 
   const [name, setName] = useState("");
@@ -26,44 +31,48 @@ const SignUpForm = ({closeModal, toggleForm}: ISignUpForm) => {
   const [password, setPassword] = useState("");
   const [formError, setFormError] = useState<boolean>(false);
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
-  const handleLastnameChange = (e: React.ChangeEvent<HTMLInputElement>) => setLastname(e.target.value);
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
-  const handlePasswordConfirmChange = (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setName(e.target.value);
+  const handleLastnameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setLastname(e.target.value);
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setPassword(e.target.value);
+  const handlePasswordConfirmChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => setPassword(e.target.value);
 
-  const userDisplayNameHandler = (userDisplayName: string | null) => 
+  const userDisplayNameHandler = (userDisplayName: string | null) =>
     dispatch(setUserDisplayName({ userDisplayName }));
-  const userEmailHandler = (userEmail: string | null) => dispatch(setUserEmail({ userEmail }));
+  const userEmailHandler = (userEmail: string | null) =>
+    dispatch(setUserEmail({ userEmail }));
 
   const handleSendSignUp = () => {
-   const auth = getAuth();
-   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      updateProfile( userCredential.user, {
-        displayName: `${name} ${lastname}`
-      })
-      .then(()=> {
-        userDisplayNameHandler(userCredential.user.displayName);
-        userEmailHandler(userCredential.user.email)
-        reset();
-        closeModal()
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        updateProfile(userCredential.user, {
+          displayName: `${name} ${lastname}`,
+        })
+          .then(() => {
+            userDisplayNameHandler(userCredential.user.displayName);
+            userEmailHandler(userCredential.user.email);
+            reset();
+            closeModal();
+          })
+          .catch((err) => {
+            console.log("...oops", err);
+          });
       })
       .catch((err) => {
-        console.log('...oops', err)
+        if (err.code === "auth/email-already-in-use") {
+          setFormError(true);
+          console.log("in use");
+        } else setFormError(false);
+        console.log("...oops", err.code);
       });
-    })
-    .catch((err) => {
-      if(err.code === 'auth/email-already-in-use'){
-        setFormError(true)
-        console.log('in use')
-
-      }
-      else
-        setFormError(false)
-        console.log('...oops', err.code)
-    });
-  }
+  };
 
   const {
     formState: { errors },
@@ -86,55 +95,56 @@ const SignUpForm = ({closeModal, toggleForm}: ISignUpForm) => {
   return (
     <div className={style.signUpForm}>
       <Input
-        name='name'
-        placeholder='Name'
+        name="name"
+        placeholder="Name"
         onInput={handleNameChange}
         register={register}
         errorText={errors.name?.message as string}
       />
       <Input
-        name='lastname'
-        placeholder='Lastname'
+        name="lastname"
+        placeholder="Lastname"
         onInput={handleLastnameChange}
         register={register}
         errorText={errors.lastname?.message as string}
       />
       <Input
-        name='email'
-        placeholder='email'
+        name="email"
+        placeholder="email"
         onInput={handleEmailChange}
         register={register}
         errorText={errors.email?.message as string}
       />
       <Input
-        name='password'
-        placeholder='password'
-        type='password'
+        name="password"
+        placeholder="password"
+        type="password"
         onInput={handlePasswordChange}
         register={register}
         errorText={errors.password?.message as string}
       />
       <Input
-        name='passwordConfirm'
-        placeholder='Password Confirm'
-        type='password'
+        name="passwordConfirm"
+        placeholder="Password Confirm"
+        type="password"
         onInput={handlePasswordConfirmChange}
         register={register}
         errorText={errors.passwordConfirm?.message as string}
       />
       <div className={style.errorWrapper}>
-        {formError && <ErrorLabel isFormLabel errorText='Sorry, user with this email already exists...'/>}
+        {formError && (
+          <ErrorLabel
+            isFormLabel
+            errorText="Sorry, user with this email already exists..."
+          />
+        )}
       </div>
-      <Button
-        clickHandler={handleSubmit(handleSendSignUp)}
-        text='Sign Up'
-      />
-      <div className={style.redirectWrapper}>Account already exists? 
-        <span 
-          onClick={toggleForm}
-          className={style.redirect}
-        >
-          &nbsp;Sign In 
+      <GoogleButton closeModal={closeModal} />
+      <Button clickHandler={handleSubmit(handleSendSignUp)} text="Sign Up" />
+      <div className={style.redirectWrapper}>
+        Account already exists?
+        <span onClick={toggleForm} className={style.redirect}>
+          &nbsp;Sign In
         </span>
       </div>
     </div>
