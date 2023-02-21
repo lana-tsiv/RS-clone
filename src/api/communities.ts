426809e12d43c917426577d1be7c35c388f786e8
Lana <tiushkievich00@mail.ru>
2023-02-21 20:52:55 +0100

    feat: add community pages

--- src/api/communities.ts
+++ src/api/communities.ts
@@ -1,4 +1,4 @@
-import { query, collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc, getDoc, doc } from "firebase/firestore";
 import { db } from "@/firebaseClient/clientApp";
 
 import { communitiesCollection } from "@/firebaseClient/collections";
@@ -11,6 +11,13 @@ export const getAllCommunities = async () => {
   return data.docs.map((item) => {
     return { ...item.data(), id: item.id };
   });
+};
+
+export const getCommunity = (id: string) => {
+  if (!id) return null;
+  const docRef = doc(communitiesCollection, id);
+  getDoc(docRef);
+  return getDoc(docRef);
 };
 
 export const createCommunity = (props: ICommunity) => {
--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -3,4 +3,5 @@ export const SINGLE_POST = "single-post";
 export const CREATE_POST = "create-post";
 export const VOTE_POST = "vote-post";
 export const COMMUNITIES = "communities";
+export const SINGLE_COMMUNITY = "community";
 export const CREATE_COMMUNITY = "create-community";
